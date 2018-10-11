546ba65e49f6115fde73ce20dac42e6187eb5073
kanekona <amausakonata1932@gmail.com>
2018-10-11 21:47:42 +0900

    Player制作中

---
+++ SilverGirlStrike/Assets/Src/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localPosition += new Vector3(5.0f, 0.0f, 0.0f);
+	}
+    /**
+     * 重力処理
+     */
+    private void Function()
+    {
+    }
+}
