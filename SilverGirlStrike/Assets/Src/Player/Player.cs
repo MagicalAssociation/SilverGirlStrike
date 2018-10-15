80b397ae7d3db152f91b0c570c0b45b02c19afbc
kanekona <amausakonata1932@gmail.com>
2018-10-15 21:51:59 +0900

    進捗上げ

--- SilverGirlStrike/Assets/Src/Player/Player.cs
+++ SilverGirlStrike/Assets/Src/Player/Player.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public SpriteRenderer sprite;
+    public Rigidbody2D rigidbody;
 	// Use this for initialization
 	void Start () {
+        rigidbody = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.localPosition += new Vector3(5.0f, 0.0f, 0.0f);
-	}
+        //this.rigidbody.velocity = new Vector2(Input.GetAxis("RStickX") * 10.0f, 0.0f);
+        this.rigidbody.velocity = new Vector2(Input.GetAxis("RStickX") * 2.0f, (9.8f / 60.0f / 60.0f * 32.0f) * -10.0f);
+
+    }
     /**
      * 重力処理
      */
