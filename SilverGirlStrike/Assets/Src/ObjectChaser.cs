bb17b846e35c899e5e051de8b3a5d5f15b54ceae
ItakuraKoji <kouzi85@gmail.com>
2018-10-27 14:10:03 +0900

    マージ、自分の作業分を追加
    ボスキャラとか、背景ループ改良版とか

---
+++ SilverGirlStrike/Assets/Src/ObjectChaser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//変更履歴
+//2018/10/26 板倉（作成）
+
+
+//特定のゲームオブジェクトに、遅れて追尾するスクリプト（相対位置で追尾位置をずらせる）
+public class ObjectChaser : MonoBehaviour {
+    public Transform chaseTarget;
+    [Range(0, 1)]
+    public float chasePower;
+    Vector3 relativePosition;
+
+
+
+    // Use this for initialization
+    void Start () {
+        //ターゲットとの相対位置を取得
+        this.relativePosition = this.transform.position - this.chaseTarget.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //相対位置分だけずらして追尾
+        this.transform.position += (this.chaseTarget.position + this.relativePosition - this.transform.position) * this.chasePower;
+	}
+}
