666f7adbf0f0a82a3a17021bf8e6e00c7c441b9e
ItakuraKoji <kouzi85@gmail.com>
2018-12-07 00:44:27 +0900

    ボスの基本動作実装
    次々に待機位置を変更してゆく、攻撃の類は明日じゃい

--- SilverGirlStrike/Assets/Src/TestCode/NewBehaviourScript.cs
+++ SilverGirlStrike/Assets/Src/TestCode/NewBehaviourScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Test : MonoBehaviour {
     //　経過時間
     private float nowTime;
 
