47c48caa0ad12de006694ccc07e5a559063121e8
kyokausui <kyoka9190@icloud.com>
2018-12-22 23:50:38 +0900

    エフェクトを消すスクリプト作り直しました

---
+++ SilverGirlStrike/Assets/Src/EffectDelete.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDelete : MonoBehaviour {
+
+    public float deleteCnt;
+    float timeCnt;
+
+	// Use this for initialization
+	void Start () {
+        timeCnt = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCnt += Time.deltaTime;
+        Delete();
+	}
+
+    void Delete()
+    {
+        if(this.deleteCnt < timeCnt)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
