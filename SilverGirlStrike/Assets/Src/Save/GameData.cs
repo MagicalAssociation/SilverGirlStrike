094236c16d9ffbb1b88dd07aef63e2a2b6a958fa
kanekona <amausakonata1932@gmail.com>
2019-01-08 02:36:33 +0900

    セーブデータ

---
+++ SilverGirlStrike/Assets/Src/Save/GameData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataParameter
+{
+    int money;
+    bool[] stageClearFlag;
+}
+public class GameData
+{
+    static GameData gameData;
+    public void Save()
+    {
+
+    }
+    public void Load()
+    {
+
+    }
+    static public void Create()
+    {
+        if(GameData.gameData == null)
+        {
+            GameData.gameData = new GameData();
+        }
+    }
+    static public GameData Get()
+    {
+        return GameData.gameData;
+    }
+}
