04302a6eb2f9c44447d5a4b57c39d41a3899ac2d
ItakuraKoji <kouzi85@gmail.com>
2019-01-20 07:57:14 +0900

    チュートリアル実装途中

---
+++ SilverGirlStrike/Assets/PopTutorial.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopTutorial : MonoBehaviour {
+
+    public Animator animator;
+
+    bool isActive;
+    string clipname;
+
+	// Use this for initialization
+	void Start () {
+        this.isActive = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        AnimatorClipInfo clipInfo = animator.GetCurrentAnimatorClipInfo(0)[0];
+        if (!this.isActive)
+        {
+            if (clipname != "disappear")
+            {
+                animator.Play("disappear");
+                clipname = "disappear";
+            }
+        }
+        else
+        {
+            if (clipname != "appear")
+            {
+                animator.Play("appear");
+                clipname = "appear";
+            }
+        }
+
+        this.isActive = false;
+        Debug.Log("c");
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("a");
+        this.isActive = true;
+    }
+}
