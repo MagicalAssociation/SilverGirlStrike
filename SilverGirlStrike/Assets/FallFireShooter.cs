fa7a6d0462700b2e0bf5c1ef4631bbea885602cc
ItakuraKoji <kouzi85@gmail.com>
2019-02-01 23:07:07 +0900

    外道仕様の火の玉完成
    エンちゃんマジ外道

---
+++ SilverGirlStrike/Assets/FallFireShooter.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//範囲内に入ったプレイヤーに、ひたすら炎を落とす人
+//プレイヤーのY座標を一定距離に保って、一定間隔で発射
+//倒せない
+public class FallFireShooter : CharacterObject {
+    [SerializeField]
+    int attackIntervalCount;
+
+    [SerializeField]
+    int chargeCount;
+
+    [SerializeField]
+    CharacterObject bulletObject;
+
+    //上方向距離
+    [SerializeField]
+    float upDistance;
+
+    //感知コリジョン
+    [SerializeField]
+    Collider2D sensor;
+
+    //感知コリジョン
+    [SerializeField]
+    CharacterManager characterManager;
+
+    int count;
+    //当たった相手
+    Collider2D[] hitResult;
+    Transform target;
+
+    public override void ApplyDamage()
+    {
+    }
+
+    public override bool Damage(AttackData attackData)
+    {
+        //ダメージとかない
+        return false;
+    }
+
+    public override void MoveCharacter()
+    {
+
+    }
+
+    public override void UpdateCharacter()
+    {
+        if (!CheckOverlapPlayer())
+        {
+            //範囲内に入ってない場合は処理しない
+            this.count = 0;
+            return;
+        }
+        this.target = this.hitResult[0].transform;
+        ++this.count;
+
+        //チャージ開始
+        if(this.count == this.attackIntervalCount)
+        {
+            Sound.PlaySE("charge1");
+
+        }
+
+        //一定間隔に達した
+        if (this.count > this.attackIntervalCount + this.chargeCount)
+        {
+            this.count = 0;
+            var obj = Instantiate<CharacterObject>(this.bulletObject, new Vector3(this.transform.position.x, this.target.transform.position.y + this.upDistance, this.target.position.z), Quaternion.identity);
+            this.characterManager.AddCharacter(obj);
+            Sound.PlaySE("impact1");
+            Sound.PlaySE("shot2");
+        }
+    }
+
+
+    bool CheckOverlapPlayer()
+    {
+        ContactFilter2D contactFilter2D = new ContactFilter2D();
+        contactFilter2D.SetLayerMask((int)M_System.LayerName.PLAYER);
+        contactFilter2D.useTriggers = true;
+        int resultLength = Physics2D.OverlapCollider(this.sensor, contactFilter2D, this.hitResult);
+        return this.hitResult.Length > 0;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        this.count = 0;
+        this.hitResult = new Collider2D[10];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
